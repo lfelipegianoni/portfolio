8fe791d6d44b0e892781400e36ccba949e816246
luis <lfelipe2305@hotmail.com.br>
2026-09-15 18:28:30 -0300

    migrate Footer CSS to CSS Modules

--- src/sections/Footer/index.jsx
+++ src/sections/Footer/index.jsx
@@ -1,4 +1,4 @@
-import "./footer-styles.css";
+import styles from "./footer.module.css";
 import phone from "../../assets/icons/phone.svg";
 import mail from "../../assets/icons/mail.svg";
 
@@ -9,28 +9,28 @@ export function Footer() {
   };
 
   return (
-    <footer id="contact" className="footer">
-      <div className="footer-container">
-        <div className="footer-col brand-col">
-          <div className="logo">
+    <footer id="contact" className={styles.footer}>
+      <div className={styles['footer-container']}>
+        <div className={`${styles['footer-col']} ${styles['brand-col']}`}>
+          <div className={styles.logo}>
             <i className="fa-solid fa-code logo-icon"></i>
-            <a href="#home" className="logo">
+            <a href="#home" className={styles.logo}>
                 <code>&lt;/&gt;</code> LuisFelipe.dev
             </a>
           </div>
         </div>
-        <div className="footer-col">
+        <div className={styles['footer-col']}>
           <h4>Navegação</h4>
-          <ul className="footer-links">
+          <ul className={styles['footer-links']}>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#skills">Skills</a></li>
             <li><a href="#projects">Projects</a></li>
           </ul>
         </div>
-        <div className="footer-col">
+        <div className={styles['footer-col']}>
           <h4>Redes sociais</h4>
-          <div className="social-icons">
+          <div className={styles['social-icons']}>
             <a href="https://www.linkedin.com/in/luis-felipe-dos-santos-gianoni-5494ab257" target="_blank" rel="noreferrer" aria-label="LinkedIn">
               <i className="devicon-linkedin-plain"></i>
             </a>
@@ -41,11 +41,11 @@ export function Footer() {
         </div>
 
         {/* Coluna 4: Informações de Contato */}
-        <div className="footer-col">
+        <div className={styles['footer-col']}>
           <h4>Informações de Contato</h4>
-          <div className="contact-list">
-            <div 
-              className="contact-item clickable" 
+          <div className={styles['contact-list']}>
+            <div
+              className={`${styles['contact-item']} ${styles.clickable}`}
               onClick={() => handleCopy('(15) 98152-0688', 'Telefone copiado!')}
             >
               <img src={phone} alt=""/>
@@ -54,7 +54,7 @@ export function Footer() {
               </span>
             </div>
             <div
-              className="contact-item clickable" 
+              className={`${styles['contact-item']} ${styles.clickable}`}
               onClick={() => handleCopy('lfelipe2305@hotmail.com.br', 'E-mail copiado!')}
             >
               <img src={mail} alt=""/>
@@ -65,7 +65,7 @@ export function Footer() {
       </div>
 
       {/* Linha de Copyright */}
-      <div className="footer-bottom">
+      <div className={styles['footer-bottom']}>
         <p>© 2026 Luis Felipe dos Santos Gianoni</p>
       </div>
     </footer>
