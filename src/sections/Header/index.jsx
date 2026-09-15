361b4f1b5132f151910c61afea892ba6f8c18a7e
luis <lfelipe2305@hotmail.com.br>
2026-09-15 18:32:26 -0300

    migrate Header CSS to CSS Modules

--- src/sections/Header/index.jsx
+++ src/sections/Header/index.jsx
@@ -1,20 +1,20 @@
-import "./header-styles.css"
+import styles from "./header.module.css"
 
 export function Header() {
   return (
-    <header>
-        <div className="container nav-container">
-            <a href="#home" className="logo">
+    <header> 
+        <div className={`${styles.container} ${styles['nav-container']}`}>
+            <a href="#home" className={styles.logo}>
                 <code>&lt;/&gt;</code> LuisFelipe.dev
             </a>
-            <ul className="nav-links">
+            <ul className={styles['nav-links']}>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#skills">Skills</a></li>
                 <li><a href="#projects">Project</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
-            <button className="lang-btn" id="#">pt-BR ▾</button>
+            <button className={styles['lang-btn']} id="#">pt-BR ▾</button>
         </div>
     </header>
   )
